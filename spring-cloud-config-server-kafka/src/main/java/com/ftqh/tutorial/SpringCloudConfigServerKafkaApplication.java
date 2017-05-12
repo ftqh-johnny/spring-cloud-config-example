64d214cc8c2f8a0bc68d66c3ef5fdbb33e1781ad
ftqh <632763745@qq.com>
2017-05-12 10:20:18 +0800

    commit config example

---
+++ spring-cloud-config-server-kafka/src/main/java/com/ftqh/tutorial/SpringCloudConfigServerKafkaApplication.java
@@ -0,0 +1,16 @@
+package com.ftqh.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class SpringCloudConfigServerKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerKafkaApplication.class, args);
+	}
+}
